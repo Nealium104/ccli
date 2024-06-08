65a31e7cccc9f51b48674c020f69bc7d42a4f8f2
Neal <neal.grindstaff@gmail.com>
2024-06-08 19:50:18 -0400

    first prototype:

--- Player.py
+++ Player.py
@@ -1,6 +1,6 @@
 class Player:
-    def __init__(self, name, role, exp = 0, age = 100, origin = 'yo mama'):
+    def __init__(self, name, role = "mage", exp = 0, age = 100, origin = 'yo mama'):
         self.name = name
         self.role = role
         self.exp = exp
-        print(f"New player {self.name} created!")
+        print(f"Player {self.name} loaded!")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import json
 from Player import Player
 
 def open_text_editor(exercise):
@@ -44,15 +45,25 @@ c              |\    \
               _\    \.- \ 
              |___.-|__.\/
              """)
-    name = input("What's your name, kind sir?\n")
-    role = input("And be you fighter, mage, or rogue?\n")
-    player1 = Player(name, role)
+    with open("./savedata", 'r') as file:
+        content = file.read()
+        if content:
+            print("Your face...yes, I remember you.")
+            data = json.loads(content)
+            player1 = Player(name=data["Name"], role=data["Role"])
+        else:
+            name = input("What's your name, kind sir?\n")
+            role = input("And be you fighter, mage, or rogue?\n")
+            player1 = Player(name, role)
+
+    print(f"Your name is {player1.name} and your role is {player1.role}.")
     def exercise():  
         exerciseChoice = input("Which exercise would you like to complete? You can choose between 'Binary Search' and 'Write a list'\n")
         if exerciseChoice == 'Binary Search':
             player1.exp += 10
             open_text_editor('binary_search')
         elif exerciseChoice == 'Write a list':
+            open_text_editor('write_a_list')
             print("A list you say?")
         else:
             print("I'm sorry, that's not an option here. Press Ctrl+C to quit, or answer the question.")
